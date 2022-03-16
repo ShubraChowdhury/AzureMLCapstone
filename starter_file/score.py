63285c9c020c24a81f03e899bfbfda3529c6bdb1
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2022-03-16 19:01:06 +0000

    Add files via upload

--- starter_file/score.py
+++ starter_file/score.py
@@ -5,8 +5,11 @@ import joblib
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'),'Maternal_Health_Risk_model.pkl')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'),'Maternal_Health_Risk_AutoML_model.pkl')
     model = joblib.load(model_path)
+    
+    #model_path = Model.get_model_path('Maternal_Health_Risk_AutoML_model')
+    #model = joblib.load(model_path)
 
 def run(data):
     try:
