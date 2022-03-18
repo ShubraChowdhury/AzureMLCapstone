dcb47e2101b7617e5ec5d6039b672a5adec9a0c5
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2022-03-18 18:23:15 +0000

    Add files via upload

--- starter_file/score.py
+++ starter_file/score.py
@@ -5,6 +5,7 @@ import json
 import sys
 import subprocess 
 from azureml.core.model import Model
+#from azureml.core.model import Model
 
 # implement pip as a subprocess this is required a my project failed joblib:
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'joblib'])
@@ -16,12 +17,13 @@ def init():
     global model
     #model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'),'Maternal_Health_Risk_AutoML_model')
     #Maternal_Health_Risk_AutoML_model
-    try:
-        model_path = Model.get_model_path('Maternal_Health_Risk_AutoML_model')
-        print("Model Path = /t",model_path)
-        model = joblib.load(model_path)
-    except Exception as e:
-        print("Init Error :/t", str(e))
+    #try:
+    #Maternal_Health_Risk_AutoML_model
+    model_path = Model.get_model_path(model_name='Maternal_Health_Risk_AutoML_model')
+    print("Model Path = \t",model_path)
+    model = joblib.load(model_path)
+    #except Exception as e:
+    #print("Init Error :/t", str(e))
     #model_path = Model.get_model_path('Maternal_Health_Risk_AutoML_model')
     #model = joblib.load(model_path)
 
@@ -29,7 +31,7 @@ def run(data):
     try:
         #data = np.array(json.loads(data))
         print("In run data try block")
-        data = pd.DataFrame(json.loads(data)['dat'])
+        data = pd.DataFrame(json.loads(data)['data'])
         result = model.predict(data)
         return result.tolist()
     except Exception as err:
